db367e31e1b538b55a3270bc52275bb6f7301c39
Cryptohat27 <spencer.bischof1@gmail.com>
2019-02-20 15:52:21 +0530

    Powershell scripts made for fun

---
+++ install-remoteapp-package-short.ps1
@@ -0,0 +1,3 @@
+﻿#Get-Service remoteregistry -ComputerName $computer | start-service
+Copy-item "\\leia\mis\New Installs\RemoteApp" -container -recurse \\$computer\c$\windows\temp\
+msiexec /i  "C:\Windows\Temp\RemoteApp\RemoteApp\Leia RA.MSI"  
